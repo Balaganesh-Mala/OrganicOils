827ad72141ab75aa92da6ccbba2db70332979df3
Balaganesh Mala <malabalaganesh@gmail.com>
2025-12-16 14:04:17 +0530

    navbar.jsx changed

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
+import Navbar from "../components/layout/Navbar.jsx";
+import Footer from "../components/layout/Footer.jsx";
 //import TopAnnouncementBar from "../components/layout/TopAnnouncementBar";
 
 
